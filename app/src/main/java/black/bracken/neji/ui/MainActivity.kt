505c51d83c4b6faf04115564312ef3e45248f0bf
blackbracken <me@bracken.black>
2020-11-19 15:21:29 +0900

    move toolbar into activity

--- app/src/main/java/black/bracken/neji/ui/MainActivity.kt
+++ app/src/main/java/black/bracken/neji/ui/MainActivity.kt
@@ -2,6 +2,10 @@ package black.bracken.neji.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import black.bracken.neji.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,6 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment)
+            ?.findNavController()!!
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(
+            navController,
+            appBarConfiguration
+        )
     }
 
 }
