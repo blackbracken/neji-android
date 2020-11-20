13102989baa59c58cd2e86b5e038b815ae772eec
blackbracken <me@bracken.black>
2020-11-21 03:37:05 +0900

    add ext to create channelFlow for firebase-rtdb easily

---
+++ app/src/main/java/black/bracken/neji/ext/DatabaseReferenceExt.kt
@@ -0,0 +1,25 @@
+package black.bracken.neji.ext
+
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.ProducerScope
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+
+@ExperimentalCoroutinesApi
+fun <T> DatabaseReference.createFlow(
+    onChanged: ProducerScope<T>.(DataSnapshot) -> Unit,
+    onCancelled: (DatabaseError) -> Unit = { error -> throw error.toException() }
+): Flow<T> = channelFlow {
+    val listener = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) = onChanged(snapshot)
+        override fun onCancelled(error: DatabaseError) = onCancelled(error)
+    }
+    this@createFlow.addValueEventListener(listener)
+    awaitClose()
+    this@createFlow.removeEventListener(listener)
+}
