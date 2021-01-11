0378e72d0fb6e6ed418ab6e3774a403640d22b7d
blackbracken <me@bracken.black>
2021-01-11 19:28:58 +0900

    add layout to search items

---
+++ app/src/main/java/black/bracken/neji/ui/searchitem/SearchItemFragment.kt
@@ -0,0 +1,11 @@
+package black.bracken.neji.ui.searchitem
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchItemFragment : Fragment() {
+
+
+
+}
---
+++ app/src/main/java/black/bracken/neji/ui/searchitem/SearchItemViewModel.kt
@@ -0,0 +1,9 @@
+package black.bracken.neji.ui.searchitem
+
+import androidx.lifecycle.ViewModel
+
+class SearchItemViewModel : ViewModel() {
+
+
+
+}
