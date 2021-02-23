e293ca17de8efcee0a0874a8482309fa52326a80
blackbracken <me@bracken.black>
2021-02-23 22:37:23 +0900

    reflect item status on ItemInfoView

--- app/src/main/java/black/bracken/neji/ui/iteminfo/ItemInfoViewModel.kt
+++ app/src/main/java/black/bracken/neji/ui/iteminfo/ItemInfoViewModel.kt
@@ -1,6 +1,18 @@
 package black.bracken.neji.ui.iteminfo
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import black.bracken.neji.ext.toLiveData
+import black.bracken.neji.model.Item
 
-class ItemInfoViewModel @ViewModelInject constructor() : ViewModel()
+class ItemInfoViewModel @ViewModelInject constructor() : ViewModel() {
+
+    private val _item: MutableLiveData<Item> = MutableLiveData()
+    val item get() = _item.toLiveData()
+
+    fun addItem(item: Item) {
+        _item.value = item
+    }
+
+}
